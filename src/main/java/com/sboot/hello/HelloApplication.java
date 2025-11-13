018d2bbca64dbd11b7e295845aff02f142c14ce5
Kim Seung Gu <agapeuni@naver.com>
2025-11-13 18:14:56 +0900

    Rename repository

--- src/main/java/com/sboot/hello/HelloApplication.java
+++ src/main/java/com/sboot/hello/HelloApplication.java
@@ -1,13 +1,18 @@
 package com.sboot.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(HelloApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
+		logger.info("HelloApplication Start");
 	}
 
 }
